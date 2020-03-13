87e71463d7355346700b462312d7536bc9be8527
db21 <you@example.com>
2020-03-13 11:37:59 +0300

    123

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+/* libs */
+
+//= libs/jquery-3.3.1.min.js
+//= libs/jquery.fancybox.min.js
+//= libs/jquery.maskedinput.min.js
+
+/* my scripts */
+
+//= partials/main.js
---
+++ src/js/partials/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("input[name='phone']").mask(" +7 (999) 999-99-99");
+
+
+
+});
